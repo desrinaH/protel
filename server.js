a8070327ff11be05ffd5617d6b6d7752f9178d55
desrinaH <desrina@thibet.asia>
2023-10-29 23:50:46 +0700

    commit 2

--- server.js
+++ server.js
@@ -19,19 +19,30 @@ db.connect();
 app.use(bodyParser.json());
 
 // Get sensors
-app.get('/api/readings/:node_id', (req, res) => {
+app.get('/readings/:node_id', (req, res) => {
     const nodeId = req.params.node_id;
-    db.query('SELECT * FROM sensors WHERE node_id = ? ORDER BY timestamp DESC LIMIT 1', [nodeId], (error, results) => {
+    db.query('SELECT * FROM sensorsread WHERE node_id = ? ORDER BY timestamp DESC LIMIT 1', [nodeId], (error, results) => {
         if (error) throw error;
         res.json(results);
     });
 });
 
+// Update sensors
+app.post('/readings', (req, res) => {
+    const { node_id, suhu1, suhu2, suhu3, suhu4, suhu5, suhu6 } = req.body;
+    const query = 'INSERT INTO sensorsread (node_id, suhu1, suhu2, suhu3, suhu4, suhu5, suhu6) VALUES (?, ?, ?, ?, ?, ?, ?)';
+    db.query(query, [node_id, suhu1, suhu2, suhu3, suhu4, suhu5, suhu6], (error, results) => {
+        if (error) throw error;
+        res.json({ message: 'Sensor updated successfully.' });
+    });
+});
+
+
 // Update actuators
-app.post('/api/actions', (req, res) => {
-    const { node_id, heater, nozzle } = req.body;
-    const query = 'INSERT INTO actuators (node_id, heater, nozzle) VALUES (?, ?, ?)';
-    db.query(query, [node_id, heater, nozzle], (error, results) => {
+app.post('/actions', (req, res) => {
+    const { node_id, status } = req.body;
+    const query = 'INSERT INTO actuators (node_id, status) VALUES (?, ?)';
+    db.query(query, [node_id, status], (error, results) => {
         if (error) throw error;
         res.json({ message: 'State updated successfully.' });
     });
