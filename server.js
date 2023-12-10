ce35388a8c3b1bc7e866b69d369889186e2d0fda
desrinaH <desrina@thibet.asia>
2023-12-10 23:57:22 +0700

    commit 5

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
+const cors = require('cors');
 
 const app = express();
 const port = 3000;
 
+app.use(cors());
 app.use(bodyParser.json());
 
 // Connect to MySQL
@@ -18,14 +20,26 @@ db.connect();
 
 app.use(bodyParser.json());
 
-// Get sensors
+// Get sensors 
 app.get('/readings/:node_id', (req, res) => {
     const nodeId = req.params.node_id;
-    db.query('SELECT * FROM sensorsread WHERE node_id = ? ORDER BY timestamp DESC LIMIT 1', [nodeId], (error, results) => {
-        if (error) throw error;
-        res.json(results);
+    db.query('SELECT avg_temp FROM sensorsread WHERE node_id = ? ORDER BY timestamp DESC LIMIT 1', [nodeId], (error, results) => {
+        if (error) {
+            // It's a good practice to handle errors properly
+            console.error(error);
+            res.status(500).json({ error: 'Internal Server Error' });
+        } else {
+            // If results array is not empty, send the avg_temp, otherwise send some default value or error
+            if (results.length > 0) {
+                const avgTemp = results[0].avg_temp; // Extract avg_temp from the first result
+                res.json({ avg_temp: avgTemp });
+            } else {
+                res.status(404).json({ error: 'No readings found' });
+            }
+        }
     });
 });
+ 
 
 // Update sensors
 app.post('/readings', (req, res) => {
@@ -69,14 +83,33 @@ app.post('/readings', (req, res) => {
 
 
 // Update actuators
-app.post('/actions', (req, res) => {
-    const { node_id, status } = req.body;
+app.post('/actions/:node_id', (req, res) => {
+    const node_id = req.params.node_id;
+    const { status } = req.body;
     const query = 'INSERT INTO actuators (node_id, status) VALUES (?, ?)';
     db.query(query, [node_id, status], (error, results) => {
         if (error) throw error;
         res.json({ message: 'State updated successfully.' });
     });
 });
+
+app.get('/actions/:node_id', (req, res) => {
+    const nodeId = req.params.node_id; // It should be req.params.node_id, not req.params.params
+    db.query('SELECT status FROM actuators WHERE node_id = ? ORDER BY timestamp DESC LIMIT 1', [nodeId], (error, results) => {
+        if (error) {
+            console.error(error);
+            res.status(500).json({ error: 'Internal Server Error' });
+        } else {
+            if (results.length > 0) {
+                const status = results[0].status; // It should be status, not avg_temp
+                res.json({ status: status });
+            } else {
+                res.status(404).json({ error: 'No actions found' });
+            }
+        }
+    });
+});
+
 
 app.listen(port, () => {
     console.log(`Server started on http://localhost:${port}`); 
