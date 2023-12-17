28ec03d86f8fdc5c879e0007561b1dc2859514a5
desrinaH <desrina@thibet.asia>
2023-12-17 10:31:51 +0700

    commit 6

--- server.js
+++ server.js
@@ -93,22 +93,77 @@ app.post('/actions/:node_id', (req, res) => {
     });
 });
 
-app.get('/actions/:node_id', (req, res) => {
-    const nodeId = req.params.node_id; // It should be req.params.node_id, not req.params.params
+app.get('/actions/:node_id', (req, res) => { //harus buat program di mcu nya untuk sending data status ke database dan mungkin bikin table baru khusus monitoring auto
+    const nodeId = req.params.node_id; 
     db.query('SELECT status FROM actuators WHERE node_id = ? ORDER BY timestamp DESC LIMIT 1', [nodeId], (error, results) => {
         if (error) {
             console.error(error);
             res.status(500).json({ error: 'Internal Server Error' });
         } else {
             if (results.length > 0) {
-                const status = results[0].status; // It should be status, not avg_temp
+                const status = results[0].status; 
                 res.json({ status: status });
             } else {
                 res.status(404).json({ error: 'No actions found' });
             }
         }
+    });    
+});
+
+app.get('/sensorsread/tensecond/:node_id', (req, res) => {
+    const nodeId = req.params.node_id;
+    db.query('SELECT * FROM sensorsread WHERE node_id = ? ORDER BY read_id DESC LIMIT 10', [nodeId], (error, results) => {
+        if (error) {
+            console.error(error);
+            res.status(500).json({ error: 'Internal Server Error' });
+        } else {
+            res.json(results);
+        }
     });
 });
+
+app.get('/sensorsread/thirtymins/:node_id', (req, res) => {
+    const nodeId = req.params.node_id;
+    db.query('SELECT * FROM sensorsread WHERE node_id = ? AND TIMESTAMPDIFF(MINUTE, timestamp, NOW()) <= 30 ORDER BY read_id DESC', [nodeId], (error, results) => {
+        if (error) {
+            console.error(error);
+            res.status(500).json({ error: 'Internal Server Error' });
+        } else {
+            res.json(results);
+        }
+    });
+});
+
+app.get('/sensorsread/hourly/:node_id', (req, res) => {
+    const nodeId = req.params.node_id;
+    const oneHourAgo = new Date(new Date().getTime() - (60*60*1000)); // Get the date and time one hour ago
+
+    // Format the date to a string that SQL can understand, if necessary
+    // This is database dependent, and the format below works for MySQL
+    const formattedDate = oneHourAgo.toISOString().slice(0, 19).replace('T', ' ');
+
+    db.query('SELECT * FROM sensorsread WHERE node_id = ? AND timestamp >= ? ORDER BY timestamp DESC', [nodeId, formattedDate], (error, results) => {
+        if (error) {
+            console.error(error);
+            res.status(500).json({ error: 'Internal Server Error' });
+        } else {
+            res.json(results);
+        }
+    });
+});
+
+app.get('/sensorsread/daily/:node_id', (req, res) => {
+    const nodeId = req.params.node_id;
+    db.query('SELECT * FROM sensorsread WHERE node_id = ? AND timestamp > NOW() - INTERVAL 24 HOUR ORDER BY timestamp DESC', [nodeId], (error, results) => {
+        if (error) {
+            console.error(error);
+            res.status(500).json({ error: 'Internal Server Error' });  
+        } else {
+            res.json(results);
+        }
+    });
+});
+
 
 
 app.listen(port, () => {
