2297297d9a0546793f9d555443565b59295ee8d8
Ahmad Hamayan Nabih <95700519+hamayanNW06@users.noreply.github.com>
2024-01-08 00:15:33 +0700

    commit buat revert

--- assets/js/tempDisplay.js
+++ assets/js/tempDisplay.js
@@ -6,20 +6,23 @@ function updateTemperatureDisplay(nodeId, avgTemp) {
         const temperatureElement = nodeElement.querySelector('h3');
         temperatureElement.textContent = `${avgTemp} °C`;
 
+<<<<<<< HEAD
         // Hitung persentase untuk suhu, asumsikan 200 sebagai suhu maksimal
         const percentage = (avgTemp / 200) * 100; 
+=======
+        // Hitung persentase untuk suhu, asumsikan 100 sebagai suhu maksimal
+        const percentage = avgTemp;
+>>>>>>> parent of a2841d9 (frontend last amin)
 
         // Tetapkan properti CSS --i untuk persentase lingkaran
         nodeElement.style.setProperty('--i', `${percentage}%`);
 
         // Tetapkan warna untuk lingkaran berdasarkan suhu
         let color;
-        if (avgTemp > 175) {
-            color = 'red'; // Warna merah untuk suhu di atas 150
-        } else if (avgTemp > 135) {
-            color = 'orange'; // Warna oranye untuk suhu 101-150
+        if (avgTemp >= 80 && avgTemp <= 100) {
+            color = 'red'; // Warna merah untuk suhu 80-100
         } else {
-            color = '#1C71F1D9'; // Warna biru sebagai default untuk suhu 100 atau kurang
+            color = '#1C71F1D9'; // Warna biru sebagai default
         }
         nodeElement.style.setProperty('--clr', color);
     } else {
@@ -30,7 +33,11 @@ function updateTemperatureDisplay(nodeId, avgTemp) {
 
 // Fungsi ini dijalankan setelah data di-fetch
 function fetchTemperatureData(nodeId) {
+<<<<<<< HEAD
     fetch(`http://192.168.156.150:3000/readings/${nodeId}`)
+=======
+    fetch(`http://10.3.146.122:3000/readings/${nodeId}`)
+>>>>>>> parent of a2841d9 (frontend last amin)
         .then(response => response.json())
         .then(data => {
             if(data.avg_temp) {
